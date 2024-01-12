f3f9751687e066e20df2037f6dfd3f498c8f401e
Elijah Buck <elijah.buck@gmail.com>
2024-01-11 18:29:26 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 echo '[[ -v GH_GH_PAT ]] && export GITHUB_TOKEN=$GH_GH_PAT' >> ~/.bashrc
+echo 'shopt -s histappend' >> ~/.bashrc
+echo 'PROMPT_COMMAND="history -a;$PROMPT_COMMAND"' >> ~/.bashrc
